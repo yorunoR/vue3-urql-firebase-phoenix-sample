caea9fbf1e764544fa208021974a8ae88a097efd
Yoshioka <sss.yoshioka@gmail.com>
2022-05-08 11:26:20 +0000

    feat: primevue導入

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,7 +1,21 @@
+// external lib
+import PrimeVue from "primevue/config";
+// vue
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+// css
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.css";
+import "primeicons/primeicons.css";
+// primevue components
+import Button from "primevue/button";
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App)
+  .use(store)
+  .use(router)
+  .use(PrimeVue)
+  .component("Button", Button)
+  .mount("#app");
