c8ef38c54975aeb7c77b6b1212b978b3bd0404bd
Yoshioka <sss.yoshioka@gmail.com>
2022-05-08 11:03:44 +0000

    feat: soft_delete追加

--- app/apps/sample/mix.exs
+++ app/apps/sample/mix.exs
@@ -40,7 +40,8 @@ defmodule Sample.MixProject do
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
       {:jason, "~> 1.2"},
-      {:swoosh, "~> 1.3"}
+      {:swoosh, "~> 1.3"},
+      {:ecto_soft_delete, "~> 2.0"}
     ]
   end
 
